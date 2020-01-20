ae5940772eed561bc226c2393a5dd1a883e24b2f
LOZADA PAREDES CRISTHIAN RAUL <DTCLOP001@cnpdes.local>
2020-01-20 08:48:58 -0500

    Cambios simples

--- src/main/java/pe/confianza/colaboradores/gcontenidos/server/controller/UsuarioController.java
+++ src/main/java/pe/confianza/colaboradores/gcontenidos/server/controller/UsuarioController.java
@@ -1,6 +1,8 @@
 package pe.confianza.colaboradores.gcontenidos.server.controller;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.bson.BsonDocument;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +17,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.google.gson.Gson;
 
 import pe.confianza.colaboradores.gcontenidos.server.bean.RequestStatus;
+import pe.confianza.colaboradores.gcontenidos.server.dao.UsuarioDao;
+import pe.confianza.colaboradores.gcontenidos.server.model.entity.Usuario;
 import pe.confianza.colaboradores.gcontenidos.server.service.AuditoriaService;
 
 @RestController
@@ -22,8 +26,12 @@ import pe.confianza.colaboradores.gcontenidos.server.service.AuditoriaService;
 @CrossOrigin(origins = { "https://200.107.154.52:6020", "http://localhost", "http://localhost:8100", "http://localhost:4200", "http://172.20.9.12:7445" })
 public class UsuarioController {
 
+	
 	@Autowired
-	private AuditoriaService auditoriaService;
+	private AuditoriaService auditoriaService;	
+	
+	@Autowired
+	private UsuarioDao usuarioDao;
 	
 	@PostMapping("/login/success")
 	public ResponseEntity<?> loginSucess(@RequestBody RequestStatus requestStatus) throws IOException {
@@ -49,5 +57,19 @@ public class UsuarioController {
 		auditoriaService.createAuditoria("001", "012", 0, "OK", BsonDocument.parse(jsonData));
 		return new ResponseEntity<RequestStatus>(requestStatus, HttpStatus.OK);
 
+	}
+	
+	@PostMapping("/login/permission")
+	public ResponseEntity<?> permission(@RequestBody  Usuario usuarioLogIn) throws IOException {
+
+		Map<String, Object> response = new HashMap<>();
+		Usuario usuarioOut = usuarioDao.findUsuarioByUsuarioBTAndAplicacion(usuarioLogIn.getUsuarioBT(), usuarioLogIn.getAplicacion());
+
+		if(usuarioOut == null) {
+			response.put("mensaje", "Usuario no existe en la base de datos!");
+			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.NOT_FOUND);
+		}
+		
+		return new ResponseEntity<Usuario>(usuarioOut, HttpStatus.OK);
 	}
 }
