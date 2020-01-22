d669568c4672dc3f40ef5a37344c21b21ad8969c
DTCLOP001 <DTCLOP001@LT991GDD17.cnpdes.local>
2020-01-22 17:15:33 -0500

    Boleta - Metodo base 64

--- src/main/java/pe/confianza/colaboradores/gcontenidos/server/controller/BoletaController.java
+++ src/main/java/pe/confianza/colaboradores/gcontenidos/server/controller/BoletaController.java
@@ -1,13 +1,16 @@
 package pe.confianza.colaboradores.gcontenidos.server.controller;
 
 import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.lang.reflect.Type;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.nio.file.Files;
 import java.util.List;
+import java.util.Base64;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -16,6 +19,8 @@ import org.apache.commons.lang.StringUtils;
 import org.bson.BsonDocument;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,12 +28,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
+import com.lowagie.text.Document;
+import com.lowagie.text.PageSize;
+import com.lowagie.text.pdf.PdfWriter;
 
 import net.sf.jasperreports.engine.JRException;
 import net.sf.jasperreports.engine.JasperExportManager;
 import net.sf.jasperreports.engine.JasperPrint;
+import pe.confianza.colaboradores.gcontenidos.server.bean.CabeceraCTS;
 import pe.confianza.colaboradores.gcontenidos.server.bean.ParamsBoleta;
 import pe.confianza.colaboradores.gcontenidos.server.bean.RequestBoleta;
+import pe.confianza.colaboradores.gcontenidos.server.bean.ResponseStatus;
 import pe.confianza.colaboradores.gcontenidos.server.model.entity.BoletaModel;
 import pe.confianza.colaboradores.gcontenidos.server.service.AuditoriaService;
 import pe.confianza.colaboradores.gcontenidos.server.service.BoletaService;
@@ -114,5 +124,77 @@ public class BoletaController {
 			String jsonData = gson.toJson(requestBoleta);
 			auditoriaService.createAuditoria("002", "005", 99, "Error al obtener boleta: " + e.getMessage(), BsonDocument.parse(jsonData));
 		}
+	}
+	
+	@RequestMapping("/boleta/text")
+	public ResponseEntity<?> obtenerBoletaString(HttpServletRequest httpServletRequest, HttpServletResponse response,@RequestBody RequestBoleta requestBoleta
+			) throws IOException, JRException {
+		Gson gson = new Gson();
+		ResponseStatus responseStatus = new ResponseStatus();
+		String base64  ="";
+		try {
+			String bodyResponse = StringUtils.EMPTY;
+			String readLine = StringUtils.EMPTY;
+
+			StringBuilder url = new StringBuilder(urlBoleta);
+			
+			URL urlForGetRequest = new URL(url.toString());
+			
+			ParamsBoleta params = new ParamsBoleta();
+			params.setIdCompania(idCompania);
+			params.setIdEmpleado(requestBoleta.getEmpleado());
+			params.setPeriodo(requestBoleta.getPeriodo());
+			String paramsOut = new Gson().toJson(params);
+			
+			HttpURLConnection conection = (HttpURLConnection) urlForGetRequest.openConnection();
+			conection.setDoOutput(true);
+			conection.setRequestMethod("POST");
+			conection.setRequestProperty("Content-Type", "application/json");
+			OutputStream os = conection.getOutputStream();
+			
+			os.write(paramsOut.getBytes());
+			os.flush();
+			os.close();
+
+			int responseCode = conection.getResponseCode();
+
+			if (responseCode == HttpURLConnection.HTTP_OK) {
+				BufferedReader in = new BufferedReader(new InputStreamReader(conection.getInputStream()));
+				StringBuffer responsePrint = new StringBuffer();
+				while ((readLine = in.readLine()) != null) {
+					responsePrint.append(readLine);
+				}
+				in.close();
+
+				bodyResponse = responsePrint.toString();
+
+				Type founderListType = new TypeToken<List<BoletaModel>>() { }.getType();
+
+				List<BoletaModel> lstDetBoleta = new Gson().fromJson(bodyResponse, founderListType);
+
+				/**
+				 * GENERANDO BOLETA EN PDF 
+				 */
+				
+				JasperPrint materiReport = _boletaService.generarPDF(httpServletRequest, lstDetBoleta);
+				response.setContentType("application/x-pdf");
+				response.setHeader("Content-disposition", "attachment; filename=BOLETA_" +requestBoleta.getEmpleado() + "_" + requestBoleta.getPeriodo() + ".pdf");
+				byte[] output =  JasperExportManager.exportReportToPdf(materiReport);
+				base64 = Base64.getEncoder().encodeToString(output);
+				responseStatus.setCodeStatus(0);
+				responseStatus.setMsgStatus("Registro creado correctamente");
+				responseStatus.setResultObj(base64);
+				/**
+				 * GUARDANDO AUDITORIA
+				 */
+				
+				String jsonData = gson.toJson(requestBoleta);
+				auditoriaService.createAuditoria("002", "005", 0, "OK", BsonDocument.parse(jsonData));
+			}
+		} catch (Exception e) {
+			String jsonData = gson.toJson(requestBoleta);
+			auditoriaService.createAuditoria("002", "005", 99, "Error al obtener boleta: " + e.getMessage(), BsonDocument.parse(jsonData));
+		}
+		return new ResponseEntity<ResponseStatus>(responseStatus, HttpStatus.OK);
 	}
 }
