de68579e79a653b8cc49f60606025793b65d716f
Luis Brando Torres <brando.torres@confianza.pe>
2020-03-02 15:24:10 -0500

    Actualizamos configuración para producción

--- src/main/java/pe/confianza/colaboradores/gcontenidos/server/service/NotificacionService.java
+++
@@ -1,9 +0,0 @@
-package pe.confianza.colaboradores.gcontenidos.server.service;
-
-import pe.confianza.colaboradores.gcontenidos.server.bean.Notification;
-import pe.confianza.colaboradores.gcontenidos.server.bean.RequestNotification;
-
-public interface NotificacionService {
-
-	Notification obtenerBodyNotificacion(Long idPublicacion, RequestNotification requestNotification);
-}
