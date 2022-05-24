5dacc85dc9213df1a99060330303ceeeaa3814f5
Kenyo Joel Pecho Ñaupari <externo5.avantica@confianza.pe>
2022-05-24 16:14:50 -0500

    consulta de resumen de vacaciones

--- src/main/java/pe/confianza/colaboradores/gcontenidos/server/service/PeriodoVacacionServiceImpl.java
+++ src/main/java/pe/confianza/colaboradores/gcontenidos/server/service/PeriodoVacacionServiceImpl.java
@@ -2,7 +2,6 @@ package pe.confianza.colaboradores.gcontenidos.server.service;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
@@ -70,12 +69,11 @@ public class PeriodoVacacionServiceImpl implements PeriodoVacacionService {
 				periodo.setDiasIndemnizables(0.0);
 				periodo.setDiasPendientesGozar(periodo.getDerecho() - periodo.getDiasGozados());
 			}
-			
 		}
 		periodo.setUsuarioModifica(usuarioOperacion.trim());
 		periodo.setFechaModifica(LocalDate.now());
+		periodoVacacionDao.save(periodo);
 		LOGGER.info("[BEGIN] actualizarPeriodo");
-		
 	}
 
 	@Override
