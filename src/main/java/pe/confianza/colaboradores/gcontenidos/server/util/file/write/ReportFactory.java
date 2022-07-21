4f737aa5071a72ddb7ce502a1f4eff6f4a4cf280
Kenyo Joel Pecho Ñaupari <externo5.avantica@confianza.pe>
2022-07-21 08:59:21 -0500

    envio de reporte

--- src/main/java/pe/confianza/colaboradores/gcontenidos/server/util/file/write/ReportFactory.java
+++ src/main/java/pe/confianza/colaboradores/gcontenidos/server/util/file/write/ReportFactory.java
@@ -1,5 +1,12 @@
 package pe.confianza.colaboradores.gcontenidos.server.util.file.write;
 
 public class ReportFactory {
+	
+	public IReport createReport(Report report) {
+		if(report.getType() == null)
+			throw new RuntimeException("No se especificó tipo de reporte");
+		
+		return null;
+	}
 
 }
